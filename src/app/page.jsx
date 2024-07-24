301aabce32674aac0abb04024b132d55a8af6939
RafaelDotorH <rafaeldotorh21@hotmail.com>
2024-07-23 22:01:15 -0600

    paguina principal front-end

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Principal from "@/components/Principal";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
+
+
+export default function HomePage() {
+  return (
+    <>
+      <Principal/>
+    </>
+  );
+}
