324c2005246b46b109a0196a2164bdf0e91baa54
AxelLaraG <axmadlar@gmail.com>
2024-07-25 20:13:26 -0600

    CorrectionDB

--- src/app/formulario/page.jsx
+++ src/app/formulario/page.jsx
@@ -23,7 +23,7 @@ export default function FormularioPlantilla() {
         correo: document.getElementById("correo").value,
         telefono: document.getElementById("telefono").value,
         asunto: document.getElementById("asunto").value,   
-
+        estatus: document.getElementById("status").value,
         fechaVen: document.getElementById("fechaInput").value, // Obtener la fecha seleccionada
       };
 
