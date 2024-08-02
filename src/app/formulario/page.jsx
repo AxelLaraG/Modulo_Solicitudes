695e6b3d4bb8da3a8f5e718c07d0b487a515c573
RafaelDotorH <rafaeldotorh21@hotmail.com>
2024-08-02 11:29:39 -0600

    modificacion de la pagina principal y del formulario, criterior de busqueda correctos

--- src/app/formulario/page.jsx
+++ src/app/formulario/page.jsx
@@ -245,36 +245,6 @@ export default function FormularioPlantilla() {
           </div>
           <div className="col-md-6">
             <div className="form-group">
-              <label htmlFor="telefono" className="fw-bold">
-                Teléfono:
-              </label>
-              <input
-                type="tel"
-                className="form-control"
-                id="telefono"
-                pattern="[0-9]{10}"
-                title="Debe contener 10 dígitos"
-                onChange={handleChange}
-                value={formData.telefono}
-              />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="form-group">
-              <label htmlFor="correo" className="fw-bold">
-                Correo:
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="correo"
-                onChange={handleChange}
-                value={formData.correo}
-              />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="form-group">
               <label htmlFor="responsable" className="fw-bold">
                 Responsable:
               </label>
@@ -345,7 +315,49 @@ export default function FormularioPlantilla() {
                 <option value="Subdirección de Programas municipales">
                   Subdirección de Programas municipales
                 </option>
+                <option value="">Otro</option>
               </select>
+              {formData.responsable === "" && (
+                <input
+                  type="text"
+                  className="form-control mt-2"
+                  placeholder="Escriba el nombre del responsable"
+                  onChange={(e) =>
+                    setFormData({ ...formData, responsable: e.target.value })
+                  }
+                  required // Asegura que se ingrese un nombre si se selecciona "Otro"
+                />
+              )}
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label htmlFor="telefono" className="fw-bold">
+                Teléfono:
+              </label>
+              <input
+                type="tel"
+                className="form-control"
+                id="telefono"
+                pattern="[0-9]{10}"
+                title="Debe contener 10 dígitos"
+                onChange={handleChange}
+                value={formData.telefono}
+              />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label htmlFor="correo" className="fw-bold">
+                Correo:
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="correo"
+                onChange={handleChange}
+                value={formData.correo}
+              />
             </div>
           </div>
           <div className="col-md-6">
